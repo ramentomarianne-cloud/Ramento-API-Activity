8ab265b00f7cb33ffa8ed25be66b2dd376e0693c
ramentomarianne-cloud <ramento.marianne@marsu.edu.ph>
2026-01-28 21:15:23 +0800

    Add files via upload

---
+++ dishModel.js
@@ -0,0 +1,31 @@
+const dishes = [
+    {
+        id: 1,
+        name: 12.99,
+        category: 'Lunch',
+        isVegetarian: false,
+    },
+    {
+        id: 1,
+        name: 'Ceasar Salad',
+        price: 8.5,
+        category: 'Starters',
+        isVegetarian: true,
+    },
+    {
+        id: 3,
+        name: 'Chocolate Lava Cake',
+        price: 8.5,
+        category: 'Dessert',
+        isVegetarian: true,
+    },
+    {
+        id: 4,
+        name: 'Grilled Salmon',
+        price: 18.0,
+        category: 'Dinner',
+        isVegetarian: false,
+    },
+];
+
+module.exports = dishes;
